5ee9b580c47e26a58685f6bc1e17c7463c0a6d60
nadunindunl <nadun1indunl@gmail.com>
2017-05-25 16:34:48 +0530

    thread added

--- AverageBandPowers.py
+++ AverageBandPowers.py
@@ -4,7 +4,8 @@ import platform
 import time
 import ctypes
 import csv
-import time
+import threading
+from PyQt5.QtCore import (QCoreApplication, QObject, QRunnable, QThread, QThreadPool, pyqtSignal)
 
 from array import *
 from ctypes import *
@@ -34,95 +35,115 @@ except Exception as e:
     print 'Error: cannot load EDK lib:', e
     exit()
 
-IEE_EmoEngineEventCreate = libEDK.IEE_EmoEngineEventCreate
-IEE_EmoEngineEventCreate.restype = c_void_p
-eEvent = IEE_EmoEngineEventCreate()
+class DataCollector(QThread):
 
-IEE_EmoEngineEventGetEmoState = libEDK.IEE_EmoEngineEventGetEmoState
-IEE_EmoEngineEventGetEmoState.argtypes = [c_void_p, c_void_p]
-IEE_EmoEngineEventGetEmoState.restype = c_int
+    IEE_EmoEngineEventCreate = libEDK.IEE_EmoEngineEventCreate
+    IEE_EmoEngineEventCreate.restype = c_void_p
+    eEvent = IEE_EmoEngineEventCreate()
 
-IEE_EmoStateCreate = libEDK.IEE_EmoStateCreate
-IEE_EmoStateCreate.restype = c_void_p
-eState = IEE_EmoStateCreate()
+    IEE_EmoEngineEventGetEmoState = libEDK.IEE_EmoEngineEventGetEmoState
+    IEE_EmoEngineEventGetEmoState.argtypes = [c_void_p, c_void_p]
+    IEE_EmoEngineEventGetEmoState.restype = c_int
 
-userID = c_uint(0)
-user   = pointer(userID)
-ready  = 0
-state  = c_int(0)
+    IEE_EmoStateCreate = libEDK.IEE_EmoStateCreate
+    IEE_EmoStateCreate.restype = c_void_p
+    eState = IEE_EmoStateCreate()
 
-alphaValue     = c_double(0)
-low_betaValue  = c_double(0)
-high_betaValue = c_double(0)
-gammaValue     = c_double(0)
-thetaValue     = c_double(0)
+    userID = c_uint(0)
+    user   = pointer(userID)
+    ready  = 0
+    state  = c_int(0)
 
-alpha     = pointer(alphaValue)
-low_beta  = pointer(low_betaValue)
-high_beta = pointer(high_betaValue)
-gamma     = pointer(gammaValue)
-theta     = pointer(thetaValue)
+    alphaValue     = c_double(0)
+    low_betaValue  = c_double(0)
+    high_betaValue = c_double(0)
+    gammaValue     = c_double(0)
+    thetaValue     = c_double(0)
 
-# set file name for the data recording file
-def setFileName(name):
-    file = open('./output/'+ name +'.csv','wb')
-    writer = csv.writer(file)
+    alpha     = pointer(alphaValue)
+    low_beta  = pointer(low_betaValue)
+    high_beta = pointer(high_betaValue)
+    gamma     = pointer(gammaValue)
+    theta     = pointer(thetaValue)
 
-def writeData():
-    channelList = array('I',[3, 7, 9, 12, 16])   # IED_AF3, IED_AF4, IED_T7, IED_T8, IED_Pz 
-    dataTypes = ["Theta", "Alpha", "Low_beta", "High_beta", "Gamma"]
-    # -------------------------------------------------------------------------
-    print "==================================================================="
-    print "Example to get the average band power for a specific channel from" \
-    " the latest epoch."
-    print "==================================================================="
+    shared = False
 
-    # -------------------------------------------------------------------------
-    if libEDK.IEE_EngineConnect("Emotiv Systems-5") != 0:
-            print "Emotiv Engine start up failed."
-            exit()
+    def run(self):
+        self.startCollect()
+    
+    def setName(self,name):
+        self.file = open('./output/'+ name +'.csv','wb')
 
-    # headlist writing
-    headList = []   
-    for i in channelList:
-        for j in dataTypes:
-            headList.append(str(i)+ " " +str(j))
+    # stop process
+    def stop(self):
+        self.shared = True
 
-    print "Theta, Alpha, Low_beta, High_beta, Gamma \n"
-    writer.writerow(headList)
-    # headlist writing is over 
+    # set file name for the data recording file
+    def startCollect(self):
+        writer = csv.writer(self.file)
+        channelList = array('I',[3, 7, 9, 12, 16])   # IED_AF3, IED_AF4, IED_T7, IED_T8, IED_Pz 
+        dataTypes = ["Time, Theta", "Alpha", "Low_beta", "High_beta", "Gamma"]
+        # -------------------------------------------------------------------------
+        print "==================================================================="
+        print "Example to get the average band power for a specific channel from" \
+        " the latest epoch."
+        print "==================================================================="
 
-    while (1):
-        state = libEDK.IEE_EngineGetNextEvent(eEvent)
+        # -------------------------------------------------------------------------
+        if libEDK.IEE_EngineConnect("Emotiv Systems-5") != 0:
+                print "Emotiv Engine start up failed."
+                exit()
+
+        # headlist writing
+        headList = []
+        for i in channelList:
+            for j in dataTypes:
+                headList.append(str(i) + " " + str(j))
+
+        print "Time, Theta, Alpha, Low_beta, High_beta, Gamma \n"
+        writer.writerow(headList)
+        # headlist writing is over
+        # start = time.time()
         
-        if state == 0:
-            eventType = libEDK.IEE_EmoEngineEventGetType(eEvent)
-            libEDK.IEE_EmoEngineEventGetUserId(eEvent, user)
-            if eventType == 16:  # libEDK.IEE_Event_enum.IEE_UserAdded
-                ready = 1
-                libEDK.IEE_FFTSetWindowingType(userID, 1);  # 1: libEDK.IEE_WindowingTypes_enum.IEE_HAMMING
-                print "User added"
-                            
-            if ready == 1:
-                listData = []
-                for i in channelList: 
-                    result = c_int(0)
-                    result = libEDK.IEE_GetAverageBandPowers(userID, i, theta, alpha, low_beta, high_beta, gamma)
-                    
-                    if result == 0:    #EDK_OK
-                        print i
-                        print "%.6f, %.6f, %.6f, %.6f, %.6f" % (thetaValue.value, alphaValue.value, 
-                                                                low_betaValue.value, high_betaValue.value, gammaValue.value)
+        while (1):
+            if self.shared == True:
+                print "break"
+                break
+            
+            state = libEDK.IEE_EngineGetNextEvent(self.eEvent)
+            
+            if state == 0:
+                eventType = libEDK.IEE_EmoEngineEventGetType(self.eEvent)
+                libEDK.IEE_EmoEngineEventGetUserId(self.eEvent, self.user)
+                if eventType == 16:  # libEDK.IEE_Event_enum.IEE_UserAdded
+                    ready = 1
+                    libEDK.IEE_FFTSetWindowingType(self.userID, 1);  # 1: libEDK.IEE_WindowingTypes_enum.IEE_HAMMING
+                    print "User added"
+                
+                                
+                if ready == 1:
+                    # end = time.time()
+                    # time = (end - start)
+                    listData = []
+                    # listData.append(time)
+                    for i in channelList: 
+                        result = c_int(0)
+                        result = libEDK.IEE_GetAverageBandPowers(self.userID, i, self.theta, self.alpha, self.low_beta, self.high_beta, self.gamma)
+                        
+                        if result == 0:    #EDK_OK
+                            print i
+                            print "%.6f, %.6f, %.6f, %.6f, %.6f" % (self.thetaValue.value, self.alphaValue.value, 
+                                                                    self.low_betaValue.value, self.high_betaValue.value, self.gammaValue.value)
 
-                        # writer.writerow(thetaValue.value, alphaValue.value,low_betaValue.value, high_betaValue.value, gammaValue.value)
-                        listData.extend((thetaValue.value, alphaValue.value,low_betaValue.value, high_betaValue.value, gammaValue.value))
-                print "\n"
-                writer.writerow(listData)    
-        elif state != 0x0600:
-            print "Internal error in Emotiv Engine ! "
-        time.sleep(0.1)
-    # -------------------------------------------------------------------------
-    file.close()
-    libEDK.IEE_EngineDisconnect()
-    libEDK.IEE_EmoStateFree(eState)
-    libEDK.IEE_EmoEngineEventFree(eEvent)
+                            # writer.writerow(thetaValue.value, alphaValue.value,low_betaValue.value, high_betaValue.value, gammaValue.value)
+                            listData.extend((self.thetaValue.value, self.alphaValue.value, self.low_betaValue.value, self.high_betaValue.value, self.gammaValue.value))
+                    print "\n"
+                    writer.writerow(listData)    
+            elif state != 0x0600:
+                print "Internal error in Emotiv Engine ! "
+            time.sleep(0.1)
+        # -------------------------------------------------------------------------
+        self.file.close()
+        libEDK.IEE_EngineDisconnect()
+        libEDK.IEE_EmoStateFree(self.eState)
+        libEDK.IEE_EmoEngineEventFree(self.eEvent)
--- TrainingMenu.py
+++ TrainingMenu.py
@@ -3,13 +3,14 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 from TrainingMenuUI import Ui_Dialog
-import AverageBandPowers as ABP
+from AverageBandPowers import DataCollector
 import time
+import threading
 
 
 class TrainingMenu(Ui_Dialog):
 	def __init__(self, dialog):
-		global chooseDirectoryText,progressBar 
+		global chooseDirectoryText,progressBar
 		Ui_Dialog.__init__(self)
 		self.setupUi(dialog)
 		chooseDirectoryText = self.chooseDirectoryText
@@ -17,15 +18,20 @@ class TrainingMenu(Ui_Dialog):
 		self.startBtn.clicked.connect(self.startBtnClicked)
 		self.endBtn.clicked.connect(self.endBtnClicked)
 		self.chooseDirectoryBtn.clicked.connect(self.chooseDirectoryBtnClicked)
+		self.dataCollector = DataCollector()
+
 		
 	def startBtnClicked(self):
 		print("Start Button Clicked")
-		ABP.setFileName(self.chooseDirectoryText.text())
-		
+		# threading.Thread(self.dataCollector.setFileName(self.chooseDirectoryText.text())).Start()
+		# self.dataCollector.start(self.chooseDirectoryText.text())
+		self.dataCollector.setName(self.chooseDirectoryText.text())
+		self.dataCollector.start()
 
 	def endBtnClicked(self):
 		print("End Button Clicked")
 		progressBar.setProperty("value", 0)
+		self.dataCollector.stop()
 
 	def chooseDirectoryBtnClicked(self):
 		print("Choose Directory Button Clicked")
